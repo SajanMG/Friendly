4e7a647012fc5a329d88f7a03350de8cc99fab1d
ekankam <eb.kankam26@gmail.com>
2022-02-01 18:54:34 -0600

    Removed onClick event from dark mode button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import { MoonIcon } from '@heroicons/react/outline'
 
-const Header = ({ isClicked, setIsClicked }) => {
+const Header = () => {
     return (
-        <header className={isClicked ? 'header dark-mode' : 'header'}>
+        <header className="header">
             <h2 className="header__logo">Where in the world?</h2>
-            <button
-                className="light-dark-mode-toggler"
-                onClick={() => setIsClicked(!isClicked)}
-            >
+            <button className="light-dark-mode-toggler">
                 <MoonIcon className="moon-icon" />
                 <p className="toggle-label">Dark Mode</p>
             </button>
